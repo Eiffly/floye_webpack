5ad661cf6ef17f2fe57da5c3cd62dfe7fdd2de5e
Eiffly <101113325+Eiffly@users.noreply.github.com>
2022-04-10 17:13:33 +0800

    4.10 2.0

---
+++ 7.总结（开发环境的配置）/src/js/index.js
@@ -0,0 +1,16 @@
+import "./iconfont.js"
+import "../css/font.css"
+import "../css/test.css"
+import "../css/test.less"
+// import test from "./test.json"
+// import "../imgs/react.png"
+// import "../imgs/vue.jpg"
+// import "../imgs/react.png"
+
+
+function add(a, b) {
+    return a + b
+
+}
+// console.log(test);
+console.log(add(10, 20));
