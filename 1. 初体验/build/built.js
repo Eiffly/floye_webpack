b0f0449481443e34bf69d3c9959d3933f7b0e0ae
Eiffly <101113325+Eiffly@users.noreply.github.com>
2022-04-10 14:42:52 +0800

    date 4.10 2.0

--- 1. 初体验/build/built.js
+++ 1. 初体验/build/built.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (function() { // webpackBootstrap
+/******/ (function () { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -14,66 +14,76 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test.json */ \"./src/test.json\");\n\r\nfunction sum(a, b) {\r\n    return a + b\r\n}\r\nconsole.log(sum(12, 32));\r\nconsole.log(_test_json__WEBPACK_IMPORTED_MODULE_0__);\n\n//# sourceURL=webpack://floye_webpack/./src/index.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test.json */ \"./src/test.json\");\n\r\nfunction sum(a, b) {\r\n    return a + b\r\n}\r\nconsole.log(sum(12, 32));\r\nconsole.log(_test_json__WEBPACK_IMPORTED_MODULE_0__);\n\n//# sourceURL=webpack://floye_webpack/./src/index.js?");
 
-/***/ }),
+        /***/
+}),
 
 /***/ "./src/test.json":
 /*!***********************!*\
   !*** ./src/test.json ***!
   \***********************/
-/***/ (function(module) {
+/***/ (function (module) {
 
-eval("module.exports = JSON.parse('{\"name\":\"floye\",\"age\":24}');\n\n//# sourceURL=webpack://floye_webpack/./src/test.json?");
+        eval("module.exports = JSON.parse('{\"name\":\"floye\",\"age\":24}');\n\n//# sourceURL=webpack://floye_webpack/./src/test.json?");
 
-/***/ })
+        /***/
+})
 
-/******/ 	});
+    /******/
+});
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
 /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
 /******/ 		if (cachedModule !== undefined) {
 /******/ 			return cachedModule.exports;
-/******/ 		}
+      /******/
+}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
 /******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
-/******/ 		};
-/******/ 	
+      /******/
+};
+/******/
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
-/******/ 	}
-/******/ 	
+    /******/
+}
+/******/
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
+/******/ 	!function () {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 		__webpack_require__.r = function (exports) {
+/******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
+        /******/
+}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
+      /******/
+};
+    /******/
+}();
+/******/
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-/******/ 	
-/******/ })()
-;
+  /******/
+  /******/
+})()
+  ;
